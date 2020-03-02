2af0cda3e78273d577e7ad379da19497d36c982b
NiallRees <niallrwoodward@gmail.com>
2020-03-02 13:50:40 +0000

    disclaimer css

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -222,7 +222,7 @@ class Dashboard extends Component {
                             </Grid>
                         </Paper>
                     </Grid>
-                    Disclaimer: This website is in no way associated with Royal Dutch Shell.
+                    <p style={{paddingLeft: "2%", width: "100%", textAlign: "left"}} >Disclaimer: This website is in no way associated with Royal Dutch Shell.</p>
 
                 </Grid>
 
